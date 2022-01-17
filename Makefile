b23edf304ca39ceea4270706afe2522255b99424
phuijse <pablo.huijse@gmail.com>
2022-01-17 18:28:07 -0300

    Usar mismo tipo de dato en la comparación (float32)

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 openblas:
-	g++ -O3 -Wall -shared -std=c++11 -fPIC $(shell python3 -m pybind11 --includes) demo.cpp -o demo$(shell python3-config --extension-suffix) -I${CONDA_PREFIX}/include/eigen3 -lopenblas -DEIGEN_USE_LAPACKE -DEIGEN_USE_BLAS
+	g++ -O3 -Wall -shared -std=c++11 -fopenmp -fPIC $(shell python3 -m pybind11 --includes) demo.cpp -o demo$(shell python3-config --extension-suffix) -I${CONDA_PREFIX}/include/eigen3 -lopenblas -DEIGEN_USE_LAPACKE -DEIGEN_USE_BLAS -DEIGEN_DONT_PARALLELIZE
 all:
 	g++ -O3 -Wall -shared -std=c++11 -fPIC $(shell python3 -m pybind11 --includes) demo.cpp -o demo$(shell python3-config --extension-suffix) -I${CONDA_PREFIX}/include/eigen3
--- profiling.py
+++ profiling.py
@@ -1,21 +1,21 @@
 import demo 
 import numpy as np
 import pickle
+import os
+
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
 diccionarioNumpy={}
 diccionarioEigen={}
-for i in [10, 50, 100, 500, 1000, 5000]:
-    A=np.random.randn(i,i)
-    B=np.random.randn(i,i)
-    time_obj_Numpy = %timeit -r10 -n5 -q -o np.dot(A,B)
-    diccionarioNumpy[i]=time_obj_Numpy
-
-    A1=np.random.randn(i,i)
-    A1= A1.astype(np.float32, order="F")
-    B1=np.random.randn(i,i)
-    B1 = B1.astype(np.float32, order="F")
-    time_obj_Eigen = %timeit -r10 -n5 -q -o demo.multMatrixByMatrix_Ref(matriz1=A1, matriz2=B1)
-    diccionarioEigen[i]=time_obj_Eigen
+for i in [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]:
+    A=np.random.randn(i, i).astype('float32')
+    B=np.random.randn(i, i).astype('float32')
+    time_obj_Numpy = %timeit -r5 -n5 -q -o np.dot(A,B)
+    diccionarioNumpy[i] = time_obj_Numpy
+    A1 = np.asfortranarray(A)
+    B1 = np.asfortranarray(B)
+    time_obj_Eigen = %timeit -r5 -n5 -q -o demo.multMatrixByMatrix_Ref(matriz1=A1, matriz2=B1)
+    diccionarioEigen[i] = time_obj_Eigen
 
 with open("profiling_np_dot.pkl", "wb") as f:
     pickle.dump(diccionarioNumpy, f)
