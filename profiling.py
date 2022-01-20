5f0ae8c9234fa37f43375d7714e8e9c108e9fd84
Pablo Huijse <pablo.huijse@gmail.com>
2022-01-19 21:35:41 -0300

    Juntar rutinas de profiling y ploteo de resultados

--- profiling.py
+++ profiling.py
@@ -1,3 +1,4 @@
+#%%
 import demo 
 import numpy as np
 import pickle
@@ -23,3 +24,44 @@ with open("profiling_np_dot.pkl", "wb") as f:
 with open("profiling_eigen.pkl", "wb") as c:
     pickle.dump(diccionarioEigen, c)
     
+# %%
+%matplotlib inline 
+import matplotlib.pyplot as plt
+import pickle
+
+with open("profiling_eigen.pkl", "rb") as f:
+    result_eigen = pickle.load(f)
+
+with open("profiling_np_dot.pkl", "rb") as f:
+    result_numpy = pickle.load(f)
+
+def unpack_results(results):
+    x = list(results.keys())
+    y = [timeit.average for timeit in results.values()]
+    yerr = [timeit.stdev for timeit in results.values()]
+    return x, y, yerr
+    
+x, y, yerr = unpack_results(result_numpy)
+fig, ax = plt.subplots(figsize=(5, 4), facecolor='w')
+ax.errorbar(x, y, yerr, label='Numpy')
+# ax.scatter(x, y)
+x, y, yerr = unpack_results(result_eigen)
+ax.errorbar(x, y, yerr, label='Eigen')
+ax.set_xlabel('Tamaño de la matriz')
+ax.set_ylabel('Tiempo promedio')
+ax.legend()
+ax.set_title('Sin linkear Openblas Pablo H')
+ax.set_xscale('log')
+ax.set_yscale('log')
+# %%
+
+# Speed-up
+fig, ax = plt.subplots(figsize=(5, 4), facecolor='w')
+x, y1, yerr1 = unpack_results(result_eigen)
+x, y2, yerr2 = unpack_results(result_numpy)
+ax.errorbar(x, np.array(y2)/np.array(y1))
+ax.set_xlabel('Tamaño de la matriz')
+ax.set_xscale('log')
+ax.set_ylabel('Speed up')
+
+# %%
